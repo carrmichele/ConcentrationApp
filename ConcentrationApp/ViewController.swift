27c7fa81b65bb217188d6edd24b956a29863f078
Caroline Gordon <carolinegordon@Carolines-MacBook-Pro.local>
2017-11-20 19:46:39 -0800

    Initial Commit

---
+++ ConcentrationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConcentrationApp
+//
+//  Created by Caroline Gordon on 11/20/17.
+//  Copyright © 2017 Caroline Gordon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
