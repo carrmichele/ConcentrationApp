784e232f3318b3730de225a821b87be59b58d186
Caroline Gordon <carolinegordon@Carolines-MacBook-Pro.local>
2017-11-24 21:41:22 -0500

    latest updates

---
+++ ConcentrationApp/Card.swift
@@ -0,0 +1,27 @@
+//
+//  Card.swift
+//  ConcentrationApp
+//
+//  Created by Caroline Gordon on 11/24/17.
+//  Copyright © 2017 Caroline Gordon. All rights reserved.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        Card.identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
